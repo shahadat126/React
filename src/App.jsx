88236b1e44407338a2a0f7632e091e75b3913561
shahadat126 <mdshahadathossainzen@gmail.com>
2024-12-14 21:07:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>I am learning React</h1>
+      <input placeholder="My Name" />
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default App;
